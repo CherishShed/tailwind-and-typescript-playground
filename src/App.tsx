3cc09c62ad00d8ae41c9ce93d59c91c49fe72ff1
CherishShed <c.shed2000@gmail.com>
2023-08-21 17:28:16 +0100

    changed to grid

--- src/App.tsx
+++ src/App.tsx
@@ -33,7 +33,7 @@ function App() {
   return (
     <>
       <Nav />
-      <div className="flex justify-around align-middle my-6 flex-wrap gap-2">
+      <div className="grid w-full gap-4 xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-1 md:grid-cols-2 px-4 ">
         {data?.map((item) => (
           <Card
             key={item.id}
--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,4 +1,4 @@
-import { TextRevise } from "../../utils/functions";
+import { TextRevise, capitalizeFirstLetter } from "../../utils/functions";
 type CardProps = {
   image: string;
   title: string;
@@ -7,20 +7,20 @@ type CardProps = {
 };
 export const Card = ({ image, title, category, description }: CardProps) => {
   return (
-    <div className="card max-h-80 h-80 overflow-hidden pb-2">
+    <div className="card max-h-full h-full overflow-hidden pb-2 max-w-full w-10/12">
       <img src={image} alt="" className="rounded-t-lg max-h-52 h-52" />
       <div className="px-2 overflow-ellipsis max-w-full border-t-2">
         <h3
-          className="text-base font-bold text-justify h-10 max-w-full pb-1 item-title"
+          className="text-base font-bold text-left h-14 max-w-full pb-4 item-title border-b-2 border-red-400"
           id="item-title"
         >
-          {TextRevise(title, 50)}
+          {TextRevise(title, 45)}
         </h3>
-        <h5 className="text-justify font-semibold overflow-hidden max-h-8 h-8 text-ellipsis">
-          {category}
+        <h5 className="text-justify font-semibold max-h-8 h-8 text-ellipsis">
+          {capitalizeFirstLetter(category)}
         </h5>
-        <p className="text-sm text-left h-9 text-ellipsis max-h-10 max-w-max">
-          {description}
+        <p className="text-sm text-left h-9 max-h-10 max-w-max">
+          {TextRevise(description, 80)}
         </p>
       </div>
     </div>
